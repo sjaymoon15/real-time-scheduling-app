5caf3ac02b30a999bf419e6875ddf5b53b10e1ab
Seungjae Moon <sjaymoon15@gmail.com>
2016-08-04 12:13:17 -0400

    finish first functional app

--- app.js
+++ app.js
@@ -4,6 +4,164 @@ angular.module('scheduleApp', ['firebase'])
 // create our main controller and get access to firebase
 .controller('mainController', function($scope, $firebase) {
   
-  // our application code will go here
-  
+	var ref = new Firebase("https://brilliant-torch-7453.firebaseio.com/days");
+	var fb = $firebase(ref);
+
+	$scope.reset = function(){
+
+		 fb.$set({
+      monday: {
+        name: 'Monday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          1100: {
+            time: '11:00am',
+            booked: false
+          },
+          0100: {
+            time: '1:00am',
+            booked: false
+          },
+          0300: {
+            time: '3:00am',
+            booked: false
+          },
+          0500: {
+            time: '5:00am',
+            booked: false
+          },
+          0700: {
+            time: '7:00am',
+            booked: false
+          }
+        }
+      },
+      tuesday: {
+        name: 'Tuesday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          1100: {
+            time: '11:00am',
+            booked: false
+          },
+          0100: {
+            time: '1:00am',
+            booked: false
+          },
+          0300: {
+            time: '3:00am',
+            booked: false
+          },
+          0500: {
+            time: '5:00am',
+            booked: false
+          },
+          0700: {
+            time: '7:00am',
+            booked: false
+          }
+        }
+      },
+      wednesday: {
+        name: 'Wednesday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          1100: {
+            time: '11:00am',
+            booked: false
+          },
+          0100: {
+            time: '1:00am',
+            booked: false
+          },
+          0300: {
+            time: '3:00am',
+            booked: false
+          },
+          0500: {
+            time: '5:00am',
+            booked: false
+          },
+          0700: {
+            time: '7:00am',
+            booked: false
+          }
+        }
+      },
+      thursday: {
+        name: 'Thursday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          1100: {
+            time: '11:00am',
+            booked: false
+          },
+          0100: {
+            time: '1:00am',
+            booked: false
+          },
+          0300: {
+            time: '3:00am',
+            booked: false
+          },
+          0500: {
+            time: '5:00am',
+            booked: false
+          },
+          0700: {
+            time: '7:00am',
+            booked: false
+          }
+        }
+      },
+      friday: {
+        name: 'Friday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          1100: {
+            time: '11:00am',
+            booked: false
+          },
+          0100: {
+            time: '1:00am',
+            booked: false
+          },
+          0300: {
+            time: '3:00am',
+            booked: false
+          },
+          0500: {
+            time: '5:00am',
+            booked: false
+          },
+          0700: {
+            time: '7:00am',
+            booked: false
+          }
+        }
+      }
+    }); 
+			
+	}
+
+	var syncObject = fb.$asObject();
+
+	syncObject.$bindTo($scope, 'days');
+
+
 });
